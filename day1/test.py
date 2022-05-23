0a0d0b4eafe95000af3cdbc497f19ae441b51f66
ParkYongJun03 <june3004@naver.com>
2022-05-23 12:31:00 +0900

    0523

---
+++ day1/test.py
@@ -0,0 +1,17 @@
+import math
+print('Hello, python!')
+a = "Hello, Python"
+print(a)
+a = 10
+print(a)
+a = False
+print(a)
+# 주석입니다. 주석은 소스 실행에 아무런 영향을 미치지 않습니다.
+# 변수명 지정
+var1 = 17
+print(var1)
+# 4var -> 변수명은 숫자로 시작할 수 없다.
+# 변수명은 영문자 또는 _로 시작, 특수문자는 _만 된다
+_ad = 19
+print(_ad)
+# 변수명은  대소문자를 구분한다
