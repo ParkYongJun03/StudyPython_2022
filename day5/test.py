a037c1f4e9ad183c071a6a841419e9d04ffedaa0
ParkYongJun03 <june3004@naver.com>
2022-05-27 12:17:02 +0900

    0527정오

--- day5/test.py
+++ day5/test.py
@@ -7,3 +7,8 @@ print('============')
 
 BASE_DIR = os.path.dirname(PRJ_ROOT)
 print(BASE_DIR)  # 프로젝트 기반 폴더 경로
+
+
+f = open('./test_console.txt', mode='w', encoding='utf-8')
+f.write('Hello World\n')
+f.close()
